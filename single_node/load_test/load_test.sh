fbfec4cf26b34c1121eeb28db23d28f0faba7bc3
liyu <liyu@shenqingtech.com>
2024-11-18 09:58:13 +0800

    code update

--- single_node/load_test/load_test.sh
+++ single_node/load_test/load_test.sh
@@ -1,6 +1,9 @@
 # 压测命令
-# ab -n 1000 -c 40 -k -r -s 10000 -T "application/json" -H "Content-Type: application/json"  -p /home/liyu/workshop/LLM/llm-inf/single_node/load_test/request_data_short.json "http://192.168.11.37:18091/v1/chat/completions"
+host="192.168.11.37"
+port="18091"
 
-# ab -n 1000 -c 5 -k -r -s 10000 -T "application/json" -H "Content-Type: application/json"  -p /home/liyu/workshop/LLM/llm-inf/single_node/load_test/request_data_mid.json "http://192.168.11.37:18091/v1/chat/completions"
+# ab -n 1000 -c 40 -k -r -s 10000 -T "application/json" -H "Content-Type: application/json"  -p /home/liyu/workshop/LLM/llm-inf/single_node/load_test/request_data_short.json "http://${host}:${port}/v1/chat/completions"
 
-ab -n 1000 -c 8 -k -r -s 10000 -T "application/json" -H "Content-Type: application/json"  -p /home/liyu/workshop/LLM/llm-inf/single_node/load_test/request_data_long.json "http://192.168.11.37:18091/v1/chat/completions"
+# ab -n 1000 -c 5 -k -r -s 10000 -T "application/json" -H "Content-Type: application/json"  -p /home/liyu/workshop/LLM/llm-inf/single_node/load_test/request_data_mid.json "http://${host}:${port}/v1/chat/completions"
+
+ab -n 1000 -c 8 -k -r -s 10000 -T "application/json" -H "Content-Type: application/json"  -p /home/liyu/workshop/LLM/llm-inf/single_node/load_test/request_data_long.json "http://${host}:${port}/v1/chat/completions"
