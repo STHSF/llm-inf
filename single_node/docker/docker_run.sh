1c191af4d35bebcec175f95c5c82463c3d4ff8f2
Yu Li <liyu52577@163.com>
2025-02-27 12:12:27 +0800

    Update docker_run.sh

--- single_node/docker/docker_run.sh
+++ single_node/docker/docker_run.sh
@@ -7,9 +7,9 @@ MODEL_PATH="/mnt/hf_models/qwen2/Qwen2-7B-Instruct"
 sudo docker run -d -it --runtime nvidia --gpus all \
         --restart=always \
         --name $PROJECT_NAME \
-	    --shm-size 24g \
-	    -v ${PROHECT_PATH}:/workshop/src \
-	    -v ${MODEL_PATH}:/workshop/models \
-	    -p 18091:8090 \
+	--shm-size 24g \
+	-v ${PROHECT_PATH}:/workshop/src \
+	-v ${MODEL_PATH}:/workshop/models \
+	-p 18091:8090 \
         harbor-prd.deepq.tech/algorithm/guosen-model:1.0-conda-py3.11-cuda-12.2.0-devel-ubi8 \
         /bin/bash /workshop/src/single_node/service_run_local.sh
