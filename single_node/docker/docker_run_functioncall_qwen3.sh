8173e3d18b4bbc9d801d6b8b3050b3925506e8cd
liyu <liyu@shenqingtech.com>
2025-06-03 20:14:53 +0800

    code update

--- single_node/docker/docker_run_functioncall_qwen3.sh
+++ single_node/docker/docker_run_functioncall_qwen3.sh
@@ -5,6 +5,8 @@ PROHECT_PATH="/home/liyu/workshop/LLM/llm-inf"
 MODEL_PATH="/mnt/hf_models/Qwen3-8B"
 MODEL_NAME="qwen3_8b"
 SERVICE_PORT=18093
+TENSOR_PARA_SIZE=1
+MAX_MODEL_LEN=20480
 
 sudo docker run -d -it --runtime nvidia --gpus all \
         --restart=always \
@@ -14,4 +16,4 @@ sudo docker run -d -it --runtime nvidia --gpus all \
 	-v ${MODEL_PATH}:/workshop/models \
 	-p ${SERVICE_PORT}:8090 \
         harbor-prd.deepq.tech/algorithm/guosen-model:vllm0.8.4-conda-py3.11-cuda-12.2.0-devel-ubi8 \
-        /bin/bash /workshop/src/single_node/service_run_local_functioncall_qwen3.sh ${MODEL_NAME}
+        /bin/bash /workshop/src/single_node/service_run_local_functioncall_qwen3.sh ${MODEL_NAME} ${TENSOR_PARA_SIZE} ${MAX_MODEL_LEN}
