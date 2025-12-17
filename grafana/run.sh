29badc51cf2351baa83aaad5487b2e18800f8b2d
liyu <liyu@shenqingtech.com>
2025-12-17 16:07:01 +0800

    多节点监控

--- grafana/run.sh
+++ grafana/run.sh
@@ -1,4 +1,4 @@
-# 参考 https://github.com/vllm-project/vllm/blob/main/examples/production_monitoring/README.md
+# 参考https://github.com/vllm-project/vllm/tree/main/examples/online_serving/prometheus_grafana
 # 参考 https://blog.csdn.net/nvd11/article/details/128030197
 
 # 修改prometheus.yml, 修改targets中的端口地址为现有vllm服务的端口
--- single_node/docker/docker_run_functioncall_qwen3.sh
+++ single_node/docker/docker_run_functioncall_qwen3.sh
@@ -4,7 +4,7 @@
 PROJECT_NAME="qwen3_llm_20480"
 PROHECT_PATH="/home/liyu/workshop/LLM/llm-inf"
 MODEL_PATH="/mnt/hf_models/Qwen3-8B"
-MODEL_NAME="qwen3_8b"
+MODEL_NAME="qwen3-8b"
 SERVICE_PORT=18093
 TENSOR_PARA_SIZE=1
 MAX_MODEL_LEN=20480
